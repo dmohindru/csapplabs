5acff4f262cadb6eab542d3dfb77640cb86b89fa
Dhruv Mohindru <dmohindru@gmail.com>
2018-07-03 09:03:53 +0530

    Solved level 3 of attack lab

--- attack/ctarget_level3_sol.d
+++ attack/ctarget_level3_sol.d
@@ -5,5 +5,5 @@ ctarget_level3_sol.o:     file format elf64-x86-64
 Disassembly of section .text:
 
 0000000000000000 <.text>:
-   0:	48 c7 c7 80 dc 61 55 	mov    $0x5561dc80,%rdi
+   0:	48 c7 c7 b0 dc 61 55 	mov    $0x5561dcb0,%rdi
    7:	c3                   	retq   
