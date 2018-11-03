7d85caa0f730b2a3038962c1ed39c9fcc3e9cac7
Dhruv Mohindru <dmohindru@gmail.com>
2018-11-03 09:13:56 +0530

    Writing function which allocates cache memory

--- cache/csim.c
+++ cache/csim.c
@@ -11,12 +11,12 @@
 #include <string.h>
 
 /* Block being represented as unsigned int which hold number of bytes */
-typedef unsigned char block;
+typedef unsigned int block;
 
 /* Single cache line */
 typedef struct {
 	unsigned char LRU_flag;
-	block block_line;
+	unsigned int tag;
 } cache_line;
 
 /* Single cache set. MIGHT NOT NEED THIS.....
@@ -27,7 +27,12 @@ cache_line *cache_set;
  * cache_memory = sizeof(cache_line) * associativity * num_of_sets 
  * And accessed as cache_memory[set_number][line_number]
  */ 
-cache_line **cache_memory;
+typedef struct {
+	unsigned int block_size;
+	unsigned int set_size;
+	cache_line **cache_line_array;
+} cache_mem;
+
 
 /* Implemenation steps
  * 1. gather command line arguments with error checking
@@ -41,14 +46,66 @@ cache_line **cache_memory;
  * 8. Print reults
  */
 
-/* Function to create cache memory */
-void create_cache_memory(int set_bits, int e_num, int block_bits)
+/* Function to create cache memory and return a pointer to it.
+ * If some thing goes wrong NULL is returned */
+cache_mem  *createCacheMemory(int set_bits, int e_num, int block_bits)
 {
+	cache_mem *cache_mem_ptr = NULL;
+	unsigned int block_size, set_size, i, j;
+	
+	/* Create cache memory */
+	cache_mem_ptr = (cache_mem *) malloc(sizeof(cache_mem));
+	if (!cache_mem_ptr)
+	{
+		printf("Error allocating cache memory structure\n");
+		return NULL;
+	}
+	printf("Created cache memory structure\n");
+	printf("sizeof cache_mem structure: %lu\n", sizeof(cache_mem));
+	printf("Allocated size of cache_mem structure: %lu\n", sizeof(cache_mem_ptr));
+	/* Calculate block_size and set_size as 2^block_bits and 2^set_bits respectively */
+	block_size = 1;
+	for (i = 0; i < block_bits; i++)
+	{
+		block_size <<= 1;
+	}
+	
+	set_size = 1;
+	for (i = 0; i < set_bits; i++)
+	{
+		set_size <<= 1;
+	}
+	
+	cache_mem_ptr->block_size = block_size;
+	cache_mem_ptr->set_size = set_size;
+	
+	/* Now the real stuff. Allocate memory for cache line */
+	//https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
+	cache_mem_ptr->cache_line_array = (cache_line **)malloc(sizeof(cache_line) * e_num * set_size);
+	if(!cache_mem_ptr->cache_line_array)
+	{
+		printf("Error allocating cache line array\n");
+		return NULL;
+	}
+	printf("Created cache line array\n");
+	
+	/* Initalize cache_memory */
+	for (i = 0; i < set_size; i++)
+	{
+		for (j = 0; j < e_num; j++)
+		{
+			cache_mem_ptr->cache_line_array[i][j].tag = 0;
+			printf("accessed tag field\n");
+			cache_mem_ptr->cache_line_array[i][j].LRU_flag = 0;
+			printf("accessed tag LRU_flag field\n");
+		}
+	}
+	return cache_mem_ptr;
 	
 }
 
 /* Function to print program usage */
-void print_usage()
+void printUsage()
 {
 	printf("./csim: Missing required command line argument\n");
 	printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
@@ -61,17 +118,17 @@ void print_usage()
 }
 int main(int argc, char *argv[])
 { 
-	// Various variable declerations
-	// I think that line[128] declaratoin can be done in a better way
+	/* Various variable declerations */
+	/* I think that line[128] declaratoin can be done in a better way */
 	char line[128], *end, *ptr, op, *trace_file_str = NULL;
 	FILE *trace_file;
 	char delimiter[] = ",";
 	int set_bits, e_num, block_bits, cmd_options, print_usage_flag, verbose_flag, bytes;
 	unsigned long address;
-	// Set various input bits to -1 as a flag if some input bits are not entered
-	set_bits = e_bits = block_bits =  -1;
+	/* Set various input bits to -1 as a flag if some input bits are not entered */
+	set_bits = e_num = block_bits =  -1;
 	
-	//Process command line arguments
+	/* Process command line arguments */
 	while ((cmd_options = getopt(argc, argv, "hvs:E:b:t:")) != -1)
 	{
 		switch(cmd_options)
@@ -105,16 +162,20 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	// Print usage if as per various conditions
+	printf("Input values: s=%u, E=%u, b=%u, t=%s\n", set_bits, e_num, block_bits, trace_file_str);
+	
+	/* Print usage if as per various conditions */
 	if ((print_usage_flag  == 1) || set_bits == -1 || e_num == -1 || block_bits == -1 || trace_file_str == NULL)
 	{
-		print_usage();
-		return 1;
+		printUsage();
+		//return 1;
 	}
 	
-	//printf("Input values: s=%u, E=%u, b=%u, t=%s\n", set_bits, e_bits, block_bits, trace_file_str);
+	cache_mem *cache_mem_ptr = createCacheMemory(set_bits, e_num, block_bits);
+	if (!cache_mem_ptr)
+		return 1;
 	
-	// Open trace file for parsing
+	/* Open trace file for parsing */
 	trace_file = fopen(trace_file_str, "r");
 	if (trace_file == NULL)
 	{
@@ -122,7 +183,13 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-	// Main loop to read trace file and simulate ram
+	
+	
+	/* print cache stats just a temp debug stuff */
+	printf("Cache set size: %u\n", cache_mem_ptr->set_size);
+	printf("Cache block size: %u\n", cache_mem_ptr->block_size);
+	
+	/* Main loop to read trace file and simulate ram */
 	while (fgets(line, sizeof(line), trace_file) != NULL)
 	{
 		if (verbose_flag == 1)
