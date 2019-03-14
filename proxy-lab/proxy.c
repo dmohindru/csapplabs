860cfa8b9937fa17efbc3d1c32d90b2b94b2949e
Dhruv Mohindru <dmohindru@gmail.com>
2019-03-14 09:39:16 +0530

    Just started proxy implementation

--- proxy-lab/proxy.c
+++ proxy-lab/proxy.c
@@ -7,6 +7,13 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
+/* Basic idea for sequential proxy
+1. Parse GET request
+2. Extract host name and query path
+3. Connect to host and send query path
+4. Receive data from server
+5. Forward data to client/brower connected
+*/
 int main()
 {
     printf("%s", user_agent_hdr);
